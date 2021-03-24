7c57cb60d8809c533af146c79ddbe6362f0c314b
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-03-24 21:45:51 +0200

    fix
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/bored-mplex.ts
+++ src/bored-mplex.ts
@@ -1,40 +1,104 @@
 import { Transform, TransformCallback, TransformOptions } from "stream";
-import { unpack } from "msgpackr";
+import { pack, unpack } from "msgpackr";
 import { Stream } from "./stream";
 import { StreamMessage } from "./types";
 
 export class BoredMplex extends Transform {
   public streams: Map<number, Stream> = new Map();
 
+  private pingInterval?: NodeJS.Timeout;
+  private pingTimeout?: NodeJS.Timeout;
+
   constructor(private onStream?: (stream: Stream) => void, opts?: TransformOptions) {
     super(opts);
+  }
+
+  enableKeepAlive() {
+    this.pingInterval = setInterval(() => {
+      this.pingTimeout = setTimeout(() => {
+        if (this.pingInterval) clearInterval(this.pingInterval);
+        this.end();
+      }, 2_000);
+      this.ping();
+      this.once("pong", () => {
+        if (this.pingTimeout) clearTimeout(this.pingTimeout);
+      });
+    }, 10_000);
+  }
+
+  disableKeepAlive() {
+    if (this.pingInterval) clearInterval(this.pingInterval);
+    if (this.pingTimeout) clearTimeout(this.pingTimeout);
+  }
+
+  ping() {
+    this.push(pack({
+      id: 0,
+      type: "ping"
+    }));
+  }
+
+  pong() {
+    this.push(pack({
+      id: 0,
+      type: "pong"
+    }));
   }
 
   _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback): void {
     const msg = unpack(chunk) as StreamMessage;
     let stream = this.streams.get(msg.id);
 
-    if (!stream) {
-      if (msg.type !== "open") {
+    switch (msg.type) {
+      case "ping": {
+        this.emit("ping");
+        this.pong();
+
         return callback();
       }
 
-      stream = new Stream(msg.id, this);
-      this.streams.set(msg.id, stream);
-      this.onStream?.(stream);
+      case "pong": {
+        this.emit("pong");
+
+        return callback();
+      }
+    }
+
+    if (!stream && msg.type === "open") {
+      stream = this.createStream(msg.id);
 
       return callback();
     }
 
-    if (msg.data) {
-      stream.push(msg.data);
+    if (!stream) {
+      return callback();
     }
 
-    if (msg.type === "close") {
-      stream.end();
-      this.streams.delete(msg.id);
+    switch (msg.type) {
+      case "data": {
+        if (msg.data) {
+          stream.push(msg.data);
+        }
+        break;
+      }
+
+      case "close": {
+        stream.end();
+        this.streams.delete(msg.id);
+
+        break;
+      }
     }
 
     callback();
+  }
+
+  protected createStream(id: number): Stream {
+    const stream = new Stream(id, this);
+
+    this.streams.set(id, stream);
+    this.onStream?.(stream);
+
+    return stream;
   }
 }
