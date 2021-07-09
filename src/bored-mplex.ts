d48f1a51d32a1e94e22358961335cdf45ccf45d8
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-07-09 08:15:12 +0300

    wip: handle backpressure
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/bored-mplex.ts
+++ src/bored-mplex.ts
@@ -3,20 +3,37 @@ import { pack, unpack } from "msgpackr";
 import { Stream } from "./stream";
 import { StreamMessage } from "./types";
 
+type BufferItem = {
+  id: number;
+  chunk: Buffer;
+  encoding: BufferEncoding;
+  callback: TransformCallback;
+};
+
 export class BoredMplex extends Transform {
   public streams: Map<number, Stream> = new Map();
 
   private pingInterval?: NodeJS.Timeout;
   private pingTimeout?: NodeJS.Timeout;
+  private streamBufferings: Map<number, BufferItem[]> = new Map();
 
   constructor(private onStream?: (stream: Stream) => void, opts?: TransformOptions) {
-    super(opts);
+    super({
+      writableHighWaterMark: 1024 * 1024 * 8,
+      readableHighWaterMark: 1024 * 1024 * 8,
+      ...opts
+    });
 
     this.on("error", () => {
       this.streams.forEach((stream) => stream.end());
     });
     this.on("finish", () => {
       this.streams.forEach((stream) => stream.end());
+    });
+    this.on("drain", () => {
+      for (const stream of this.streams.values()) {
+        stream.emit("sessionDrained");
+      }
     });
   }
 
@@ -93,7 +110,9 @@ export class BoredMplex extends Transform {
     switch (msg.type) {
       case "data": {
         if (msg.data) {
-          stream.push(msg.data);
+          this.pushWithBackpressure(stream, msg.data, encoding, callback);
+        } else {
+          callback();
         }
         break;
       }
@@ -101,16 +120,78 @@ export class BoredMplex extends Transform {
       case "close": {
         stream.end();
         this.streams.delete(msg.id);
+        callback();
 
         break;
       }
+
+      default: {
+        callback();
+      }
+    }
+  }
+
+  private pushWithBackpressure(stream: Stream, chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
+    if (stream.writableEnded) {
+      callback();
+
+      return;
     }
 
-    callback();
+    if (this.streamBufferings.has(stream.id)) {
+      const buffer = this.streamBufferings.get(stream.id);
+
+      buffer?.push({id: stream.id, chunk, encoding, callback});
+
+      return;
+    }
+
+    const pushed = stream.push(chunk, encoding);
+
+    if (!pushed) {
+      const buffer = this.streamBufferings.get(stream.id);
+
+      if (!buffer) {
+        callback();
+
+        return;
+      }
+
+      buffer.push({id: stream.id, chunk, encoding, callback});
+      this.streamBufferings.set(stream.id, buffer);
+    } else {
+      callback();
+    }
   }
 
   protected createStream(id: number): Stream {
     const stream = new Stream(id, this);
+
+    stream.on("drain", () => {
+      const buffer = this.streamBufferings.get(id);
+
+      if (!buffer) {
+        return;
+      }
+
+      this.streamBufferings.delete(id);
+
+      for (const item of buffer) {
+        const stream = this.streams.get(item.id);
+
+        if (!stream) {
+          item.callback();
+
+          continue;
+        }
+
+        this.pushWithBackpressure(stream, item.chunk, item.encoding, item.callback);
+      }
+    });
+
+    stream.on("finish", () => {
+      this.streamBufferings.delete(id);
+    });
 
     this.streams.set(id, stream);
     this.onStream?.(stream);
--- src/stream.ts
+++ src/stream.ts
@@ -2,10 +2,26 @@ import { pack } from "msgpackr";
 import { Duplex } from "stream";
 import { BoredMplex } from "./bored-mplex";
 
+type BufferItem = {
+  chunk: Buffer;
+  encoding: BufferEncoding;
+  callback: WriteCallback;
+};
+
+type WriteCallback = (error?: Error | null) => void;
+
 export class Stream extends Duplex {
+  private buffer?: BufferItem[];
+
   constructor(public id: number, private session: BoredMplex) {
     super({
-      emitClose: true
+      emitClose: true,
+      writableHighWaterMark: 1024 * 1024 * 8,
+      readableHighWaterMark: 1024 * 1024 * 8
+    });
+
+    this.on("sessionDrained", () => {
+      this.flushBuffer();
     });
 
     this.on("finish", () => {
@@ -31,15 +47,50 @@ export class Stream extends Duplex {
     //
   }
 
-  public _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
-    if (this.session.writableEnded) return;
+  public _write(chunk: any, encoding: BufferEncoding, callback: WriteCallback): void {
+    this.pushWithBackpressure(chunk, encoding, callback);
+  }
 
-    this.session.push(pack({
+  private pushWithBackpressure(chunk: Buffer, encoding: BufferEncoding, callback: WriteCallback) {
+    if (this.session.writableEnded) {
+      return;
+    }
+
+    if (this.buffer !== undefined) {
+      this.buffer.push({ chunk, encoding, callback });
+
+      return;
+    }
+
+    const pushed = this.session.push(pack({
       id: this.id,
       type: "data",
       data: chunk
-    }));
+    }), encoding);
 
-    callback();
+    if (!pushed) {
+      this.buffer = [{ chunk, encoding, callback }];
+
+      console.log("cannot push to session", this.id, this.buffer.length, this.session);
+    } else {
+      callback();
+    }
+  }
+
+  private flushBuffer() {
+    if (!this.buffer) {
+      this.buffer = undefined;
+
+      return;
+    }
+    console.log("DRAIN STREAM", this.id, this.buffer.length);
+
+    const buffer = this.buffer;
+
+    this.buffer = undefined;
+
+    for (const item of buffer) {
+      this.pushWithBackpressure(item.chunk, item.encoding, item.callback);
+    }
   }
 }
